46dde1906b2bc12337825ef11c6829b2481082c4
Yevhenii Ushakov <y.ushakov@seedium.io>
2025-05-07 13:16:00 +0300

    fix(core): build

--- superset-frontend/plugins/plugin-chart-table/src/DataTable/DataTable.tsx
+++ superset-frontend/plugins/plugin-chart-table/src/DataTable/DataTable.tsx
@@ -159,7 +159,7 @@ export default typedMemo(function DataTable<D extends object>({
     // understand pageSize = 0
     sortBy: sortByRef.current,
     pageSize: initialPageSize > 0 ? initialPageSize : resultsSize || 10,
-  };
+  }; 
   const defaultWrapperRef = useRef<HTMLDivElement>(null);
   const globalControlRef = useRef<HTMLDivElement>(null);
   const paginationRef = useRef<HTMLDivElement>(null);
