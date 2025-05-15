a37d6eb2982c64a008222ed5660cbf4525d626f3
Yevhenii Ushakov <y.ushakov@seedium.io>
2025-05-15 12:14:36 +0300

    fix(core): build

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -93,7 +93,6 @@ class CeleryConfig:
 CELERY_CONFIG = CeleryConfig
 
 FEATURE_FLAGS = {"ALERT_REPORTS": True}
-HTML_SANITIZATION = False
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 WEBDRIVER_BASEURL = "http://superset:8088/"
 # The base URL for the email report hyperlinks.
